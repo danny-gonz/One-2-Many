4bd217cdd4b4045659e7e11b2dde3aa099cfb55f
luis rivera <luisriv84@yahoo.com>
2014-11-21 22:08:56 -0800

    pulling new layout

---
+++ Web App/Controllers/MyCrewAPIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_App.Models;
+
+namespace Web_App.Controllers
+{
+    [RoutePrefix("api/MyCrew")]
+    public class MyCrewAPIController : ApiController
+    {
+    //    [Route("MyCrew"), HttpPost]
+    //    public HttpResponseMessage Add(MyCrewAddRequestModel model)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+
+    //            return Request.CreateResponse(HttpStatusCode.OK, ModelState);
+
+
+    //        }
+    //        else
+    //        {
+    //            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+
+            }
+
+
+        }
+//    }
+//}
