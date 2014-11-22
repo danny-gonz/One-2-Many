3b2942202ac729d3d207c9e1528c30a1c8b1380b
dannyh81 <danielholzman28@gmail.com>
2014-11-21 22:12:02 -0800

    homepage

---
+++ Web App/Controllers/HomePageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_App.Controllers
+{
+    //[RoutePrefix("splashpage")]
+
+    public class HomePageController : Controller
+    {
+        [Route("homepage")]
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
