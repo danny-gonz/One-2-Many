360403059c2adab640df870332dd3d50d5dab6b3
richacos <ranthony97@gmail.com>
2014-11-21 21:57:58 -0800

    Get latest files.

---
+++ Web App/Controllers/ConfirmationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_App.Controllers
+{
+    //[RoutePrefix("Confirm")]
+
+    public class ConfirmationController : Controller
+    {
+        // GET: Confirmation
+        //[Route("Notice"), HttpGet]
+        public ActionResult Notification()
+        {
+            return View();
+        }
+    }
+}
