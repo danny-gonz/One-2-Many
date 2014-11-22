7902a8891019f14e81fbae7df7edea424da2689a
danny-gonz <dpg2012@hotmail.com>
2014-11-22 10:30:44 -0800

    conect view model and make get with notifications page

--- Web App/Controllers/ConfirmationController.cs
+++ Web App/Controllers/ConfirmationController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using Web_App.Models;
 
 namespace Web_App.Controllers
 {
-    //[RoutePrefix("Confirm")]
+    
 
     public class ConfirmationController : Controller
     {
         // GET: Confirmation
-        //[Route("Notice"), HttpGet]
-        public ActionResult Notification()
+        
+        public ActionResult Notification([FromUri]  NotificationViewModel model)
         {
             return View();
         }
