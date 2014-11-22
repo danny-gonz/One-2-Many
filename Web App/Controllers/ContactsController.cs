d199c341004044e32ff07464a927edd9aaa58174
danny-gonz <dpg2012@hotmail.com>
2014-11-21 22:02:26 -0800

    controller begin view

---
+++ Web App/Controllers/ContactsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_App.Controllers
+{   [RoutePrefix ("contacts")]
+    public class ContactsController : Controller
+    {
+       [Route ("")]
+        public ActionResult Index()
+        {
+            return View("contacts");
+        }
+    }
+}
